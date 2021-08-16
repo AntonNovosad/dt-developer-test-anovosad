d3c5819c23317de3c5c7a6babe6d5301c505abe3
Anton Novosad <noobito.2011@gmail.com>
2021-08-16 15:17:24 +0300

    added PictureDialog for open any pictures in original size

---
+++ src/main/java/com/vizor/test/gallery/PictureDialog.java
@@ -0,0 +1,17 @@
+package com.vizor.test.gallery;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PictureDialog {
+
+    public Component dialog(String name, ImageIcon imageIcon, JLabel label) {
+        JDialog jDialog = new JDialog();
+        jDialog.setTitle(name);
+        jDialog.setSize(imageIcon.getIconWidth(), imageIcon.getIconHeight());
+        jDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        jDialog.add(label);
+        jDialog.setVisible(true);
+        return jDialog;
+    }
+}
