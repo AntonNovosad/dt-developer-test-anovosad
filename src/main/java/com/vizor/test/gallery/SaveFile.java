910473e5068156842007fb4308809ced000b7052
Anton Novosad <noobito.2011@gmail.com>
2021-08-16 15:18:12 +0300

    added SaveFile class for add new pictures in gallery

---
+++ src/main/java/com/vizor/test/gallery/SaveFile.java
@@ -0,0 +1,43 @@
+package com.vizor.test.gallery;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class SaveFile {
+
+    public Component execute() {
+        JPanel jPanel = new JPanel();
+        JButton jButton = new JButton("Add new picture");
+        jButton.setFont(new Font("Verdana", Font.BOLD, 15));
+        jPanel.add(jButton);
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JFileChooser jFileChooser = new JFileChooser();
+                jFileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                jFileChooser.setDialogTitle("Open Picture");
+                jFileChooser.setFileFilter(new FileFilter() {
+                    @Override
+                    public boolean accept(File pathname) {
+                        String name = pathname.getName().toLowerCase();
+                        return pathname.isFile() &&
+                                name.endsWith(".png") ||
+                                name.endsWith(".jpg");
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return ".png , .jpg";
+                    }
+                });
+                jFileChooser.showOpenDialog(jPanel);
+            }
+        });
+        return jPanel;
+    }
+}
